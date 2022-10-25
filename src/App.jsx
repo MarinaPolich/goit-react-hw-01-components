ab58d29881002734c4e10c1b5850b61c40d3c185
MarinaPolich <marina.polich@gmail.com>
2022-10-26 01:59:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import user from "./data/user.json";
-import data from "./data/data.json";
-import friends from "./data/friends.json";
-import transactions from "./data/transactions.json";
-import Profile from "./components/Profile/Profile";
-import Statistics from "./components/Statistics/Statistics";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import React from 'react';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 function App() {
   return (
@@ -22,8 +22,8 @@ function App() {
       />
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} /> */}
-      <FriendList friends={friends} />;
-      <TransactionHistory items={transactions} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
